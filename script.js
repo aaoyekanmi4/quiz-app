81dc7fccfb72615781c72310a628ac7414746104
Arit Oyekan <aaoyekanmi4@gmail.com>
2019-09-28 15:29:56 -0700

    checkAnswer can compare to correct

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const QUIZ = [{question: "What was the name of Tony's yacht?", 
               answers: ["Fugazi", "Stugots", "Stunad", "WiseGuys"], 
               correct: "Stugots"}, 
+              
               {question: "What is the name of the final episode?", 
               answers: ["The Diner", "Calling All Cars", "Pine Barrens", "Made in America"], 
               correct: "Made in America"} 
@@ -26,12 +27,13 @@ function startQuiz(){
 function displayQuestion(index =0, score=0){
     let question = QUIZ[index].question;
     let answers = QUIZ[index].answers;
+    let id = index;
 
     let currentQ = index + 1;
     
     let answersHTMLArr = answers.map(answer => 
         `<div class="answer ">
-                <input type="radio" name="answer" value="${answer}">
+                <input type="radio" name="answer"  value="${answer}">
                 <br>
                 <p>${answer}</p>
         </div>`);
@@ -39,8 +41,8 @@ function displayQuestion(index =0, score=0){
 
 
 
-    let questionHTML = `<form>
-                            <section class="quiz-content">
+    let questionHTML = `<form action="">
+                            <section class="quiz-content" data-question-index="${index}" >
                                 <p class="current">Current:${currentQ} out of ${QUIZ.length}</p>
                                 <p class="number-corr">Number Correct:${score}</p>
                                 <h3 class="question">${question}</h3>
@@ -53,7 +55,20 @@ function displayQuestion(index =0, score=0){
 
 //Check to see if an answer is correct or incorrect with submit button pressed.
 function checkAnswer(){
-    console.log("checkAnswer ran");
+    //When submit button is clicked, get value of checked item and compare to correct answer.
+    $(".quiz").on('click', ".submit", function(event) {
+        event.preventDefault();
+        let checkedAnswer = $("input[name='answer']:checked").val();
+        let index = $('.quiz-content').data('question-index');
+        let correctAnswer = QUIZ[index].correct;
+        if (checkedAnswer === correctAnswer){
+            alert("You are correct!");
+        }
+        else {
+            alert("Sorry, that's incorrect");
+        }
+    });
+    
 
 }
 
@@ -82,6 +97,7 @@ function endQuiz(){
 //Run the quiz 
 function handleQuiz() {
  startQuiz();
+ checkAnswer();
  
 }
 
