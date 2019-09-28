967376627ab1d404e6e77ad15d7844ee521422b6
Arit Oyekan <aaoyekanmi4@gmail.com>
2019-09-28 13:27:26 -0700

    added script.js added function stubs to script.js

---
+++ script.js
@@ -0,0 +1,47 @@
+//Start Quiz by clicking Start button 
+function startQuiz(){
+    
+}
+
+//Render question to the html page
+function displayQuestion(){
+
+}
+
+//Check to see if an answer is correct or incorrect with submit button pressed.
+function checkAnswer(){
+
+}
+
+//Code to perform if the answer was correct 
+function correct() {
+
+}
+
+//Code to perform if the answer was incorrect
+function incorrect(){
+
+}
+
+//Move to next question
+function changeQuestion(){
+
+}
+
+//End quiz if last question
+function endQuiz(){
+
+}
+
+//Run the quiz 
+function handleQuiz() {
+   startQuiz();
+   displayQuestion();
+   checkAnswer();
+   correct();
+   incorrect();
+   changeQuestion();
+   endQuiz();
+}
+
+$(handleQuiz);
