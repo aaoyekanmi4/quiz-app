3ed633c845f62fd1fe981c73f1cb2ceadecfd68a
Arit Oyekan <aaoyekanmi4@gmail.com>
2019-09-30 08:17:35 -0700

    moved data into QUIZ.js

--- script.js
+++ script.js
@@ -1,82 +1,14 @@
-const QUIZ = [{
-              question: "Which iterative method returns an array with values of arr greater than 6?", 
-              arr: "const arr = [5,6,7,8,9, 10]",
-              answers: ["arr.filter(num => num > 6);", "arr.find(num => num > 6);", "arr.reduce(num => num > 6);", "arr.map(num => num > 6);"], 
-              correct: "arr.filter(num => num > 6);", 
-              documentation:"https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter",
-              topic: "Array.prototype.filter",
-              gotRight: false},
-              
-              {question: "Which method will NOT mutate arr?", 
-              arr: "let arr = [11,12,13,14,15]",
-              answers: ["arr.pop();", "arr.shift();", "arr.push(16);", "arr.concat(16);"], 
-              correct: "arr.concat(16);",
-              documentation:"https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/concat",
-              topic: "Array.prototype.concat()",
-              gotRight: false},
-            
-              {question: "Which method can I use to add an element to the front of the array?", 
-              
-              arr: "let arr = [3,4,5,6,7]",
-              answers: ["arr.push(2);", "arr.shift(2);", "arr.pop(2);", "arr.unshift(2);"], 
-              correct: "arr.unshift(2);",
-              documentation:"https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/unshift",
-              topic: "Array.prototype.unshift()",
-              gotRight: false},
-
-              {question: "Find the expression where bool === true.", 
-              
-              arr: "const arr = [1,2,3,4,5]",
-              answers: [
-                  "let bool = arr.filter(num => num < 6);", 
-                  "let bool = arr.forEach(num => num === 5);", 
-                  "let bool = arr.some(num => num > 2);", 
-                  "let bool = arr.every(num => num > 2);"
-                ], 
-              correct: "let bool = arr.some(num => num > 2);",
-              documentation:"https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/some",
-              topic: "Array.prototype.some()",
-              gotRight: false},
-
-              {question: "How can I console.log() the name of each cow?", 
-              
-              arr: "const milkcows = ['Daisy','Mary','Bertha','Sue']",
-              answers: [
-                  "for (cow in milkcows){console.log(cow)}", 
-                  "for (cow in array){console.log(cow)}", 
-                  "for (milkcow of farm){console.log(cow)}", 
-                  "for (cow of milkcows){console.log(cow)}", 
-                ], 
-              correct: "for (cow of milkcows){console.log(cow)}",
-              documentation:"https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...of",
-              topic: "for ...of", 
-              gotRight: false,
-              endScore:0}
-            
-              
-
-            
-            
-            
-            ];
-
-
-
-
-
 
 //Start Quiz by clicking Start button 
 function startQuiz(){
-    $("form").off('click', ".end", end)
-    $(".quiz").on('click', '.start', start);
-  
-   
-}
-function start () {
+    $('.start').click(function (event) {
+    //Change any previously correct answers from true to false
     QUIZ.forEach(question => question.gotRight = false);
+    //Hide .start-quiz section or .end-quiz section
     $(this).parent().hide();
+    //Show the first question
     displayQuestion();
-};
+});
 
 
 
@@ -85,7 +17,7 @@ function displayQuestion(index =0, score=0){
     let question = QUIZ[index].question;
     let answers = QUIZ[index].answers;
     let arr = QUIZ[index].arr;
-    $(".quiz").off('click', '.start', start);
+   
     $(".quiz").off('click', ".next", goToNext );
     $("form").on('click', ".answer-button", checkIfDone );
 console.log(index)
@@ -244,7 +176,7 @@ function end() {
       links += `<li class="review-topic"><a href='${question.documentation}'>${question.topic}</a><li>`;
     }
 }
-    //let links = listOfLinks.join();
+    
     $("form").html(`<section class="end-quiz">
     <h1> Thanks for Taking the Quiz</h1> 
     <br>
