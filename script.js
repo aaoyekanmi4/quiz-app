3e03cc270cce6ef86cb8612381bfdfc47a421528
Arit Oyekan <aaoyekanmi4@gmail.com>
2019-10-02 11:48:09 -0700

    used form submit for questions and show and hide

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 //Start Quiz by clicking Start button 
 function startQuiz(){
-    $('.start').click(function () {
+    $('body').on('click', '.start', function () {
     //Change any previously correct answers from true to false
     QUIZ.forEach(question => question.gotRight = false);
     //Hide .start-quiz section or .end-quiz section
-    $(this).parent().hide();
-   
+    $(".start-quiz, .end-quiz").hide();
+    //Show question area
+    $(".quiz-content").show();
+  
     //Show the first question
     displayQuestion();
  });
@@ -14,12 +16,11 @@ function startQuiz(){
 //Generate HTML list items out of answers and return the HTML string
 function generateAnswersHTML (answers){
     let answersHTMLArr = answers.map(answer => 
-    `<li class="answer ">
+    `<div class="answer ">
         <input type="radio" name="answer"  id ="${answer}" value="${answer}" required>
-        <br>
-        <br>
-        <label for="${answer}">${answer}</p>
-    </li>`);
+       
+        <label for="${answer}">${answer}</label>
+    </div>`);
     let answersHTML = answersHTMLArr.join('');
     return answersHTML;
 }
@@ -27,30 +28,31 @@ function generateAnswersHTML (answers){
 //Generate HTML for a single question at a time
 function generateQuestionsHTML(index, currentQ, score, question, arr, answersHTML){
     let questionHTML = `
-    <section class="quiz-content" data-question-index="${index}" >
-        <button class="next">Next <i class="fas fa-arrow-alt-circle-right"></i>
-        </button>
+     
+    <fieldset class="question-field" data-question-index="${index}" >
+        
         <p class="current">Current:${currentQ} out of ${QUIZ.length}</p>
         <p class="number-corr" data-score = "${score}" >Number Correct:${score}</p>
         
-        <h3 class="question">${question}</h3>
+        <legend class="question">${question}</legend>
         <p class="arr-box">${arr}</p>
         <p class="end"><button class="end">View Results</button></p>
-        <button class="answer-button">Submit</button>
         
-    </section><ul class="answers-container">${answersHTML}</ul>`
+        <section class="answers-container">${answersHTML}<button class="next">Next </button>
+     <input type="submit" class="answer-button"></section></fieldset>`
     return questionHTML;
 }
 
 //Render question to the html page
 function displayQuestion(index =0, score=0){
     //Turn off click listener for next question
-    $(".quiz").off('click', ".next", goToNext );
+    $(".quiz-content").off('click', ".next", goToNext );
     //Turn on click listener for submitting an answer
-    $("form").on('click', ".answer-button", handleSubmission );
+    $("form").on('submit', handleSubmission );
        
     //From QUIZ.js, get question to ask, answers, example array, and current question  
     let answers = QUIZ[index].answers;
+   
     let answersHTML = generateAnswersHTML(answers);
     let arr = QUIZ[index].arr;
     let question = QUIZ[index].question;
@@ -80,7 +82,7 @@ function incorrect(){
     //Add text by wrong answer radio button
     $("input[name='answer']:checked").after("<span class='wrong'> Sorry, that was incorrect</span>");
     //Get correct answer
-    let index = $('.quiz-content').data('question-index');
+    let index = parseInt($('.question-field').data('question-index'));
     let correctAnswer = QUIZ[index].correct;
     //Highlight correct answer
     $(`input[name='answer'][value='${correctAnswer}']`).closest('div').addClass('correct-answer');
@@ -90,10 +92,11 @@ function incorrect(){
 
 //Check to see if an answer is correct or incorrect
 function checkAnswer(checkedAnswer){      
-    let index = $('.quiz-content').data('question-index');
+    let index = parseInt($('.question-field').data('question-index'));
     let score = $(".number-corr").data('score'); 
     let correctAnswer = QUIZ[index].correct;
-
+    
+   console.log(checkedAnswer);
     if (checkedAnswer === correctAnswer){
         //Set gotRight attribute to true in QUIZ.
         QUIZ[index].gotRight = true;
@@ -110,15 +113,19 @@ function checkAnswer(checkedAnswer){
 //Check to see if quiz is over
 function isItOver(){
     //Get current index, score, and index of last question
-    let index = $('.quiz-content').data('question-index');
+    let index = parseInt($('.question-field').data('question-index'));
     let score = $(".number-corr").data('score');
     let finalIndex = QUIZ.length -1;
     
     //If on last question show "View Results Button" and set final score to current score
     if (index === finalIndex){
        QUIZ[index].endScore = score;
+    //Remove click listener from form
+    $("form").off('submit',  handleSubmission);
+       $(".end").show();
+
         $(".answer-button").hide();
-        $(".end").show();
+        
         endQuiz();
     }
     //Move to next question
@@ -138,6 +145,7 @@ function handleSubmission() {
 
     //check if user entered an answer
     let checkedAnswer = $("input[name='answer']:checked").val();
+    console.log(checkedAnswer);
     if (!checkedAnswer) {
             alert("Please select an option");
         }
@@ -155,21 +163,21 @@ function handleSubmission() {
 //Display next question in QUIZ
 function goToNext () {
     //Increment index of QUIZ
-    let index = $('.quiz-content').data('question-index');
+    let index =parseInt($('.question-field').data('question-index'));
     index ++;
     //Get score to display on next screen
     let score = $(".number-corr").data('score');
     //Hide the "Next Button"
     $(".next").hide();
     //Turn click listener off for submitting form
-    $("form").off('click', ".answer-button", handleSubmission);
+    $("form").off('submit',  handleSubmission);
     //Display the next question in QUIZ
     displayQuestion(index, score);
 }
 
 //Move to next question when "Next" clicked
 function nextQuestion(){
-    $(".quiz").on('click', ".next", goToNext);
+    $(".quiz-content").on('click', ".next", goToNext);
 
 }
 
@@ -181,7 +189,7 @@ function generateEndHtml(score, endMessage){
         links += `<li class="review-topic"><a target ="_blank" href='${question.documentation}'>${question.topic}</a><li>`;
       }
      }
-    let endHTML = `<section class="end-quiz">
+    let endHTML = `
         <h1> Thanks for Taking the Quiz</h1> 
         <br>
         <p class="show-score">Your Score: ${score}/${QUIZ.length}</p>
@@ -190,13 +198,21 @@ function generateEndHtml(score, endMessage){
         <br>
         <p>Take the quiz again!</p>
         <p class=".start-align"><button class="start">Start Quiz</button><p>
-    </section>`;
+   `;
     return endHTML;
 
 }
 
 //Add data to ending message to display to user. Allow them to start again
 function end() {
+    event.preventDefault();
+    //Show end quiz screen
+    $(".end-quiz").show();
+    //Hide Start and question area
+   
+    $(".quiz-content").hide();
+   
+
     //Get score to display
    let score =  QUIZ[QUIZ.length-1].endScore;
 
@@ -217,7 +233,7 @@ function end() {
    //Generate ending screen HTML using helper function
    let endHTML = generateEndHtml(score, endMessage);
    //Append the HTML to the page
-    $("form").html(endHTML);
+    $(".end-quiz").html(endHTML);
     
    
     };
@@ -225,7 +241,7 @@ function end() {
 
 //End quiz when "View Results" button clicked
 function endQuiz(){
-    $(".quiz").on('click', ".end", end)
+    $(".quiz-content").on('click', ".end", end)
 }
 
 //Run the quiz 
